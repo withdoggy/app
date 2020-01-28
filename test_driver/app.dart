e64c5113ffcf710098e199ed10be7e43a2349590
Wojciech Wójcik <wojtaswojcik@gmail.com>
2020-01-28 15:46:04 +0100

    inital project

---
+++ test_driver/app.dart
@@ -0,0 +1,8 @@
+import 'package:flutter_driver/driver_extension.dart';
+import 'package:app/main.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  enableFlutterDriverExtension();
+  runApp(MyApp());
+}
